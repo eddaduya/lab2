035526bcfe59c068fbf9cf5b1043c87f146612d1
eddaduya <eddaduya@student.apc.edu.ph>
2024-01-24 14:55:07 +0800

    PHP Lesson 8. Functions

--- week8/index.php
+++ week8/index.php
@@ -2,11 +2,10 @@
 <html lang="en">
 <body>
     <?php
-    $i = 1;
-    while ($i < 10) {
-      echo $i;
-      $i++;
+    function name() {
+        echo "Erika Alessandra Daduya :)";
     }
-    ?>
+
+    name(); 
 </body>
 </html>
